551238cf416b67caec291ddde7196082b6f0b5df
Paul Milham <domreinalabaster@gmail.com>
2016-08-24 07:10:18 -0600

    👮 add joi validation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ const Path = require('path');
 const Hapi = require('hapi');
 const Hoek = require('hoek');
 const Inert = require('inert');
+const Joi = require('joi');
+const MySql = require ('mysql');
 
-const server = new Hapi.Server(
-  {
+const connection = MySql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'stakey',
+});
+
+const server = new Hapi.Server({
   connections: {
     routes: {
       files: {
@@ -14,12 +22,12 @@ const server = new Hapi.Server(
       },
     },
   },
-}
-);
-server.connection({ port: 3000 });
+});
+server.connection({port: 3000});
 
 server.register(Inert, () => {});
 
+// SERVER STATIC ASSETS
 server.route({
   method: 'GET',
   path: '/public/{param*}',
@@ -55,7 +63,7 @@ server.route({
   handler: function (request, reply) {
     // reply.view('index');
     reply.view('calling', { fortune: 'You will eat a cookie' });
-  }
+  },
 });
 
 // view all callings
@@ -65,7 +73,7 @@ server.route({
   handler: function (request, reply) {
     // reply.view('index');
     reply.view('callings', { fortune: 'You will eat a cookie' });
-  }
+  },
 });
 
 // view a specific calling
@@ -76,7 +84,7 @@ server.route({
     // look up all info for calling and put into read only mode
     // reply.view('index');
     reply.view('calling', { fortune: 'You will eat a cookie' });
-  }
+  },
 });
 
 // admin (add/remove/edit users, update wards/branches)
@@ -86,8 +94,157 @@ server.route({
   handler: function (request, reply) {
     // reply.view('index');
     reply.view('admin', { fortune: 'You will eat a cookie' });
-  }
+  },
 });
+
+// API CALLS
+// create a new calling
+server.route({
+  method: 'POST',
+  path: '/callings',
+  handler: function (request, reply) {
+    console.log("payload: ");
+    console.log(request.payload);
+    // shove all that garbage into the database
+    connection.connect();
+
+    connection.query(`
+      INSERT INTO stakey.callingForms (
+        firstName,
+        middleName,
+        lastName,
+        position,
+        currentPositionHolder,
+        recommendedBy,
+        recommendedDate,
+        submittedBy,
+        submittedDate,
+        reason,
+        templeWorthy,
+        wardId,
+        currentCalling,
+        currentCallingStartDate,
+        phone,
+        altPhone,
+        spouse,
+        spouseCurrentCalling,
+        bishopConsulted,
+        bishopConsultedDate,
+        highCouncilRepConsulted,
+        highCouncilRepConsultedDate,
+        stakePresidencyApproval,
+        stakePresidencyApprovalDate,
+        highCouncilApproval,
+        highCouncilApprovalDate,
+        interviewed,
+        interviewer,
+        interviewDate,
+        interviewResults,
+        interviewNotes,
+        incumbentReleased,
+        incumbentReleaser,
+        incumbentReleaseDate,
+        sustained,
+        sustainer,
+        sustainedDate,
+        setApart,
+        setApartBy,
+        setApartDate
+      ) VALUES (
+        'paul',
+        'timothy',
+        'milham',
+        'Pokemaster',
+        'Ash Ketchum',
+        'Harold Shewell',
+        CURDATE(),
+        'Harold Shewell',
+        CURDATE(),
+        'You know its his destiny.',
+        1,
+        1,
+        'Hymnbook straightener',
+        CURDATE(),
+        '555-5555',
+        '555-5555',
+        'Harmony Milham',
+        'Young Womens President',
+        1,
+        CURDATE(),
+        1,
+        CURDATE(),
+        1,
+        CURDATE(),
+        1,
+        CURDATE(),
+        1,
+        'Jesse',
+        CURDATE(),
+        'Unable to steal Pokemon.',
+        'Must plan another heist.',
+        1,
+        'James',
+        CURDATE(),
+        1,
+        'Brock',
+        CURDATE(),
+        1,
+        'Misty',
+        CURDATE()
+      )
+    `, function(err, rows, fields) {
+      if (err) throw err;
+      connection.end();
+    });
+
+  },
+  config: {
+    validate: {
+      payload: {
+        firstName: Joi.string().max(255),
+        middleName: Joi.string().max(255),
+        lastName: Joi.string().max(255),
+        // position: Joi.string().max(255),
+        // currentPositionHolder: Joi.string().max(255),
+        // recommendedBy: Joi.string().max(255),
+        // recommendedDate: Joi.number().integer().positive(),
+        // submittedBy: Joi.string().max(255),
+        // submittedDate: Joi.number().integer().positive(),
+        // reason: Joi.string().max(255),
+        // templeWorthy: Joi.number().integer().positive().max(1),
+        // wardId: Joi.number().integer().positive().max(255),
+        // currentCalling: Joi.string().max(255),
+        // currentCallingStartDate: Joi.number().integer().positive(),
+        // phone: Joi.string().max(255),
+        // altPhone: Joi.string().max(255),
+        // spouse: Joi.string().max(255),
+        // spouseCurrentCalling: Joi.string().max(255),
+        // bishopConsulted: Joi.number().integer().positive().max(1),
+        // bishopConsultedDate: Joi.number().integer().positive(),
+        // highCouncilRepConsulted: Joi.number().integer().positive().max(1),
+        // highCouncilRepConsultedDate: Joi.number().integer().positive(),
+        // stakePresidencyApproval: Joi.number().integer().positive().max(1),
+        // stakePresidencyApprovalDate: Joi.number().integer().positive(),
+        // highCouncilApproval: Joi.number().integer().positive().max(1),
+        // highCouncilApprovalDate: Joi.number().integer().positive(),
+        // interviewed: Joi.number().integer().positive().max(1),
+        // interviewer: Joi.string().max(255),
+        // interviewDate: Joi.number().integer().positive(),
+        // interviewResults: Joi.string().max(1024),
+        // interviewNotes: Joi.string().max(1024),
+        // incumbentReleased: Joi.number().integer().positive().max(1),
+        // incumbentReleaser: Joi.string().max(255),
+        // incumbentReleaseDate: Joi.number().integer().positive(),
+        // sustained: Joi.number().integer().positive().max(1),
+        // sustainer: Joi.string().max(255),
+        // sustainedDate: Joi.number().integer().positive(),
+        // setApart: Joi.number().integer().positive().max(1),
+        // setApartBy: Joi.string().max(255),
+        // setApartDate: Joi.number().integer().positive(),
+      },
+    },
+  },
+})
 
 server.start((err) => {
     if (err) {
